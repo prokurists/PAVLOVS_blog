7056572e946fdff3b905e4ed4e929fd737925c32
prokurists <71398178+prokurists@users.noreply.github.com>
2020-09-16 22:46:21 +0300

    .

--- resources/views/portfolio.php
+++ resources/views/portfolio.php
@@ -1,9 +1,9 @@
 <div class="container">
   <div class="row">
-    <div class="column-33">
+    <div class="col-sm-3">
     <img src="../resources/images/portfolio.png" class="img-fluid" id="images">
     </div>
-    <div class="column-66">
+    <div class="col-sm-9">
     <h1 class="large-font" style="color:MediumSeaGreen;"><b>Kāpēc Tu te esi ?</b></h1>
       <p> You should buy this app because lorem ipsum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
   </div>
