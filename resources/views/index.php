42316154b5c858e738c148252715377e667f17d6
prokurists <71398178+prokurists@users.noreply.github.com>
2020-09-16 22:06:06 +0300

    .

--- resources/views/index.php
+++ resources/views/index.php
@@ -1,8 +1,8 @@
 <div class="container">
   <div class="row">
     <div class="column-66">
-      <h1 class="xlarge-font"><b>Kāpec Tu te esi ?/b></h1>
-      <p><span style="font-size:36px">Tāpēc, ka man patīk tas ko es daru un es nevēlos apstāties!</span></p>
+      <h1 class="large-font" style="color:MediumSeaGreen;"><b>Kāpēc Tu te esi ?</b></h1>
+      <p> You should buy this app because lorem ipsum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
     </div>
     <div class="column-33">
         <img src="../resources/images/1.jpg" width="335" height="471">
