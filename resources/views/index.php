d0bb7f98053f5161ac21fb306fa8c57ddc3e8ce9
prokurists <71398178+prokurists@users.noreply.github.com>
2020-09-16 22:04:03 +0300

    .

--- resources/views/index.php
+++ resources/views/index.php
@@ -1,10 +1,8 @@
 <div class="container">
   <div class="row">
     <div class="column-66">
-      <h1 class="xlarge-font"><b>The App</b></h1>
-      <h1 class="large-font" style="color:MediumSeaGreen;"><b>Why buy it?</b></h1>
-      <p><span style="font-size:36px">Take photos like a pro.</span> You should buy this app because lorem ipsum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-      <button class="button">Download Application</button>
+      <h1 class="xlarge-font"><b>Kāpec Tu te esi ?/b></h1>
+      <p><span style="font-size:36px">Tāpēc, ka man patīk tas ko es daru un es nevēlos apstāties!</span></p>
     </div>
     <div class="column-33">
         <img src="../resources/images/1.jpg" width="335" height="471">
