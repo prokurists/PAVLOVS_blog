9113b629c55e68e5998d2cfdefd9b53dbd6fba6a
prokurists <71398178+prokurists@users.noreply.github.com>
2020-09-20 22:29:24 +0300

    .
    .

--- resources/navi.php
+++ resources/navi.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-default">
+<!-- <nav class="navbar navbar-default">
   <div class="container-fluid">
     <div class="navbar-header">
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
@@ -20,5 +20,28 @@
       <a class="nav-link" href="/contact">Kontaktinformācija</a>
     </li>
     </div>
+  </div> -->
+</nav>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+    <a class="navbar-brand" href="#">Hidden brand</a>
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+    <li class="nav-item">
+      <a class="nav-link" href="/">Galvene</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="/portfolio">Porftolio</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="/about">Par mani</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="/contact">Kontaktinformācija</a>
+    </li>
+    </ul>
   </div>
 </nav>
