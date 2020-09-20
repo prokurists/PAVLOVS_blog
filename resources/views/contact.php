72c94f1d086ea89f96a235579dec808867b61f09
prokurists <71398178+prokurists@users.noreply.github.com>
2020-09-20 21:00:27 +0300

    .
    .

--- resources/views/contact.php
+++ resources/views/contact.php
@@ -5,12 +5,9 @@
     </div>
     <div class="col-sm-9">
     <h1 class="large-font" style="color:MediumSeaGreen;"><b>Kontaktinformācija</b></h1>
-      <button type="button" class="btn btn-fb"><i class="fab fa-facebook-f pr-1"></i> Facebook</button>
-      <button type="button" class="btn btn-li"><i class="fab fa-linkedin-in pr-1"></i> Linkedin</button>
-      <button type="button" class="btn btn-ins"><i class="fab fa-instagram pr-1"></i> Instagram</button>
-      <button type="button" class="btn btn-git"><i class="fab fa-github pr-1"></i> Github</button>
-      <button type="button" class="btn btn-email"><i class="fas fa-envelope pr-1"></i> Email</button>
-      <button type="button" class="btn btn-whatsapp"><i class="fab fa-whatsapp pr-1"></i> Whatsapp</button>
+      <a href="#" class="fa fa-facebook"></a>
+      <a href="#" class="fa fa-linkedin"></a>
+      <a href="#" class="fa fa-instagram"></a>
 </div>
   </div>
 </div>
