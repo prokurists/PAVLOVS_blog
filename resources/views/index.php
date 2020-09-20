874ebf5c6ddc9ed305bc89800657e1604b19784b
prokurists <71398178+prokurists@users.noreply.github.com>
2020-09-20 20:28:10 +0300

    .
    .

--- resources/views/index.php
+++ resources/views/index.php
@@ -1,11 +1,8 @@
-<div class="container">
-  <div class="row">
-    <div class="col-sm-9">
-      <h1 class="large-font" style="color:MediumSeaGreen;"><b>Blogs</b></h1>
-      <p><?php require ("posts.php"); ?></p>
-    </div>
-    <div class="col-sm-3">
-        <img src="../resources/images/galvene.png" class="img-fluid" id="images">
-    </div>
+<div class="card mb-3">
+  <img class="card-img-top" src="..." alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
   </div>
 </div>
