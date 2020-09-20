5e66a8b6110dad46f49de9fa5fffcf641193b3be
prokurists <71398178+prokurists@users.noreply.github.com>
2020-09-20 22:10:39 +0300

    .
    .

--- resources/views/index.php
+++ resources/views/index.php
@@ -1,25 +1,18 @@
 <h1 class="display-1" id="heading">Jaunumi / Blogs</h1>
-<div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="..." alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
+<div class=card-deck>
+<!-- First Card -->
+<div class="card">
+<div class="card-title>Title here</div>
+<div class="card-body">Body here</div>
 </div>
-<div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="..." alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
+<!-- Second Card -->
+<div class="card">
+<div class="card-title>Title here</div>
+<div class="card-body">Body here</div>
 </div>
-<div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="..." alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
+<!-- Second Card -->
+<div class="card">
+<div class="card-title>Title here</div>
+<div class="card-body">Body here</div>
+</div>
 </div>
