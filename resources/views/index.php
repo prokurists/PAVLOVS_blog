a4e7bb079cdecaff7e5e9b0e0e639cfd3facbb97
prokurists <71398178+prokurists@users.noreply.github.com>
2020-09-20 23:06:47 +0300

    .
    .

--- resources/views/index.php
+++ resources/views/index.php
@@ -4,14 +4,14 @@
     <p class="card-text"><?php require ("posts.php"); ?></p>
     <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
 </div>
-</div>
+</div><br><br>
 <div class="card">
 <div class="card-body">
     <h5 class="card-title">Pirmā pieredze programmēšanā</h5>
     <p class="card-text"><?php require ("posts.php"); ?></p>
     <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
 </div>
-</div>
+</div><br><br>
 <div class="card">
 <div class="card-body">
     <h5 class="card-title">Pirmā pieredze programmēšanā</h5>
