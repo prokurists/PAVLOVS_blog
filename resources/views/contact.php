937c56f4fb47a2ee84491635baa64ec2161904cb
prokurists <71398178+prokurists@users.noreply.github.com>
2020-09-20 22:44:44 +0300

    .
    .

--- resources/views/contact.php
+++ resources/views/contact.php
@@ -1,1 +1,15 @@
 <h1 id="heading">Kontaktinformācija</h1>
+<h3> Ja vēlies kontaktēties, lūdzu aizpildi formu </h3>
+<form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleFormControlTextarea1">Piezīmes</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+<a href="">Facebook</a> | <a href="">Whatsapp</a> | <a href="">LinkedIn</a> | <a href="">Instagram</a>
