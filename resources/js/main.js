804cea8f8d8ae5abf2fd414874143094fd45d936
prokurists <71398178+prokurists@users.noreply.github.com>
2020-09-22 00:01:08 +0300

    .
    .

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,14 +1,6 @@
-function print_about (){
-    var x = document.getElementById("about_message");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
 function parbaudit_formu(){
-    var vards = document.forms["forma"]["vards"].value;
-    var teksts = document.forms["forma"]["teksts"].value;
+    var vards = document.forms["form"]["email"].value;
+    var teksts = document.forms["form"]["textarea"].value;
 
     if (vards == ""){
         alert("Jāaizpilda vārds");
@@ -20,10 +12,4 @@ function parbaudit_formu(){
         alert("Nedrīkst būt tukši lauki!")
     }
     
-}
-
-$is_Disabled = "disabled";
-
-function checkForm(x){
-return false;
 }
--- resources/views/contact.php
+++ resources/views/contact.php
@@ -1,12 +1,12 @@
 <h3> Ja vēlies sazināties ar mani, lūdzu aizpildi formu </h3>
 <form>
-  <div class="form-group" onsubmit="return parbaudit_formu()">
+  <div class="form-group" value="form" onsubmit="return parbaudit_formu()">
     <label for="email">E-pasta adrese</label>
-    <input type="email" class="form-control">
+    <input type="email" value="email"class="form-control">
   </div>
   <div class="form-group">
     <label for="textarea">Piezīmes</label>
-    <textarea class="form-control" rows="3"></textarea>
+    <textarea class="form-control" value="textarea" rows="3"></textarea>
   </div>
   <div class="form group">
   <label for="captcha">Captcha <?php captcha(); echo "Cik ir " . $first_Number . " + " . $second_Number. " = " . $total_Sum; ?></label>
