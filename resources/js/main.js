584a24f41ae2dcf14b7e8b55a6257b2dfeef0eb1
prokurists <71398178+prokurists@users.noreply.github.com>
2020-09-22 00:52:03 +0300

    .
    .

--- resources/js/main.js
+++ resources/js/main.js
@@ -5,13 +5,13 @@ function check_Form(){
     var total_client = document.forms["form"]["total_sum_client"].value;
 
     if (total_client !== total_real){
-      alert("Summa nesakrīt!");  }
+        alert("Summa nesakrīt!"); exit;  }
 
     else if (email == ""){
-        alert("Jāaizpilda vārds");    }
+        alert("Jāaizpilda vārds"); exit;   }
 
     else if (text_area == ""){
-        alert("Jāaizpilda teksts");    }
+        alert("Jāaizpilda teksts"); exit;   }
 
     
 }
