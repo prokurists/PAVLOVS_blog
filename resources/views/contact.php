c817dfe365b24215abacbbd52535f8045be149c9
prokurists <71398178+prokurists@users.noreply.github.com>
2020-09-21 23:55:43 +0300

    .
    .

--- resources/views/contact.php
+++ resources/views/contact.php
@@ -11,13 +11,13 @@
   <div class="form group">
   <label for="captcha">Captcha <?php echo "Cik ir " . $first_Number . " + " . $second_Number. " = " . $total_Sum; ?></label>
   
-  <button type="submit" class="btn btn-primary">Submit</button>
+  <button onSubmit="return captcha();" class="btn btn-primary">Submit</button>
 </form>
 
 <?php
-
+  function captcha() {
   $first_Number = rand(0,10);
   $second_Number = rand(0,10);
   $total_Sum = $first_Number + $second_Number;
-
+  }
 ?>
