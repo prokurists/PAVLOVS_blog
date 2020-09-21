0f54a51a6947e9443e14d209d6a83aff417f8552
prokurists <71398178+prokurists@users.noreply.github.com>
2020-09-21 23:53:28 +0300

    .
    .

--- resources/views/contact.php
+++ resources/views/contact.php
@@ -1,6 +1,6 @@
 <h3> Ja vēlies sazināties ar mani, lūdzu aizpildi formu </h3>
 <form>
-  <div class="form-group" onSubmit="return checkForm()">
+  <div class="form-group" onSubmit="return parbaudit_formu()">
     <label for="email">E-pasta adrese</label>
     <input type="email" class="form-control">
   </div>
@@ -9,7 +9,15 @@
     <textarea class="form-control" rows="3"></textarea>
   </div>
   <div class="form group">
-  <label for="captcha">Captcha</label>
+  <label for="captcha">Captcha <?php echo "Cik ir " . $first_Number . " + " . $second_Number. " = " . $total_Sum; ?></label>
   
-  <button type="submit" class="btn btn-primary" <?php $is_Disabled; ?> >Submit</button>
+  <button type="submit" class="btn btn-primary">Submit</button>
 </form>
+
+<?php
+
+  $first_Number = rand(0,10);
+  $second_Number = rand(0,10);
+  $total_Sum = $first_Number + $second_Number;
+
+?>
