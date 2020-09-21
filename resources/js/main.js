c775b1b41483105bdf089b87f19fce6e0c51e9ee
prokurists <71398178+prokurists@users.noreply.github.com>
2020-09-22 00:28:55 +0300

    .
    .

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,8 +1,8 @@
 function parbaudit_formu(){
     var vards = document.forms["form"]["email"].value;
     var teksts = document.forms["form"]["textarea"].value;
-    var summa_client = document.forms["form"]["total_sum_client"].value;
-    var summa_real = document.forms["form"]["total_sum_real"].value;
+    // var summa_client = document.forms["form"]["total_sum_client"].value;
+    // var summa_real = document.forms["form"]["total_sum_real"].value;
 
     if (summa_client !== summa_real){
       alert("Summa nesakrīt!");  }
