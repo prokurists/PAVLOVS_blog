9e6208983e1c8b88369387da27ffd35e2cb54148
prokurists <71398178+prokurists@users.noreply.github.com>
2020-09-22 00:19:04 +0300

    .
    .

--- resources/js/main.js
+++ resources/js/main.js
@@ -2,7 +2,9 @@ function parbaudit_formu(){
     var vards = document.forms["form"]["email"].value;
     var teksts = document.forms["form"]["textarea"].value;
     var summa = document.forms["form"]["total_sum"].value;
-
+    var first_number = "<?php echo $first_Number; ?>";
+    var second_number = "<?php echo $second_Number; ?>";
+    var total_sum = "<?php echo $total_Sum; ?>";
 
     if (vards == ""){
         alert("Jāaizpilda vārds");
