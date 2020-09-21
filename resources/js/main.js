301cb4f503d581dd67d96a3c38a61dd8c8e6f21e
prokurists <71398178+prokurists@users.noreply.github.com>
2020-09-22 00:31:03 +0300

    .
    .

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,13 +1,13 @@
 function parbaudit_formu(){
     var vards = document.forms["form"]["email"].value;
     var teksts = document.forms["form"]["textarea"].value;
-    // var summa_client = document.forms["form"]["total_sum_client"].value;
-    // var summa_real = document.forms["form"]["total_sum_real"].value;
+    var summa_client = document.forms["form"]["total_sum_client"].value;
+    var summa_real = document.forms["form"]["total_sum_real"].value;
 
-    // if (summa_client !== summa_real){
-    //   alert("Summa nesakrīt!");  }
+    if (summa_client !== summa_real){
+      alert("Summa nesakrīt!");  }
 
-if (vards == ""){
+    else if (vards == ""){
         alert("Jāaizpilda vārds");    }
 
     else if (teksts == ""){
