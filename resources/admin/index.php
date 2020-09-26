61ec9eb1a40896540f1fe0df386076d26e4399ca
prokurists <71398178+prokurists@users.noreply.github.com>
2020-09-26 16:43:56 +0300

    .
    .

--- resources/admin/index.php
+++ resources/admin/index.php
@@ -1,38 +1,3 @@
-<?php
-session_start();
-
-if ($_SESSION["admin"] != "admin"){
-  if(isset($_POST['admin_login'])){
-    $admin_email = test_input($_POST["admin_email"]);
-    $admin_password = test_input($_POST["admin_password"]);
-
-    echo $admin_email;
-    
-    if ($admin_password = "admin" & $admin_email = "admin@admin"){
-      echo "Esi ielogojies";
-      $_SESSION["admin"] = "admin";}
-      else{
-        echo "Parole vai lietotājvārds ir nepareizs";
-      }
-    } 
-
-  ?>
-
-<body class="text-center">
-    <form class="form-signin" method="POST" name="admin_login">
-      <h1 class="h3 mb-3 font-weight-normal">Lūdzu ielogojies</h1>
-      <label for="inputEmail" class="sr-only">E-pasta adrese</label>
-      <input type="email" name="admin_email" id="inputEmail" class="form-control" placeholder="E-pasta adrese" required autofocus>
-      <label for="inputPassword" class="sr-only">Password</label>
-      <input type="password" name="admin_password" id="inputPassword" class="form-control" placeholder="Parole" required>
-      <div class="checkbox mb-3">
-
-      </div>
-      <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-    </form>
-
-<?php  } else {    ?>
-
   <form name="admin_add"  method="POST" action="<?php echo $_SERVER['REQUEST_URI']; ?>">
   <div class="form-group">
     <label for="text">Posta nosaukums</label>
@@ -44,15 +9,3 @@ if ($_SESSION["admin"] != "admin"){
   </div> 
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
-
-
-  <?php
-}
-  if(isset($_POST['admin_add'])){
-  $post_name = test_input($_POST["post_name"]);
-  $post_text = test_input($_POST["post_text"]);
-
-
-  }
-
-?>
