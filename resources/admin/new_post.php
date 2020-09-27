e8678b06ef949b0f236e6a8912d783e87c20b235
prokurists <71398178+prokurists@users.noreply.github.com>
2020-09-27 21:29:50 +0300

    .
    .

--- resources/admin/new_post.php
+++ resources/admin/new_post.php
@@ -9,7 +9,7 @@
 
 
 
-<form name="admin_add"  method="POST" action="">
+<form name="admin_add"  method="POST" action="/post/new">
   <div class="form-group">
     <label for="text">Posta nosaukums</label>
     <input type="text" name="name" class="form-control">
