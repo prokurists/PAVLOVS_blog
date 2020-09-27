663468a76fb1b50ddc3530c5cc6ef9d8b459fa15
prokurists <71398178+prokurists@users.noreply.github.com>
2020-09-27 21:36:47 +0300

    .
    .

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
   <title>Aleksejs portfolio</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">  <link rel="stylesheet" type="text/css" href="resources/style/style.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">  
+  <link rel="stylesheet" type="text/css" href="./resources/style/style.css">
 </head>
   <body>
       <?php require __DIR__ . '/resources/navi.php';?>
@@ -27,7 +28,7 @@
   
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
   
-  <script src="resources/js/main.js"></script>
+  <script src="./resources/js/main.js"></script>
 
   </body>
 </html>
