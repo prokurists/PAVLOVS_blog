a0d25d311c257278de1638c5cf2392b03b22b09b
prokurists <71398178+prokurists@users.noreply.github.com>
2020-09-28 16:07:03 +0300

    Update script.php

--- resources/script.php
+++ resources/script.php
@@ -7,15 +7,5 @@ function test_input($data) {
     return $data;
   }
 
-function deletePost($data){
-    $delete_post = "DELETE FROM posts WHERE id='".$data."'";
-    if ($conn->query($delete_post) === TRUE) {
-  echo "Record deleted successfully";
-} else {
-  echo "Error deleting record: " . $conn->error;
-}
-
-$conn->close();
-
-}     
+     
 ?>
