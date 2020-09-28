49df82675ded54fd65ea6463de7cec6d792d18ba
prokurists <71398178+prokurists@users.noreply.github.com>
2020-09-28 12:21:39 +0300

    Update index.php

--- resources/admin/index.php
+++ resources/admin/index.php
@@ -14,7 +14,14 @@
 if (isset($_POST["new_post"])){
     $post_name = test_input($_POST["name"]);
     $post_text = test_input($_POST["post_text"]);
-    
-        echo "Paldies, " . $post_name . ". Tavs teksts: " . $post_text . " ir nosūtīts!"; }
+  
+$sql = "INSERT INTO MyGuests (name, text)
+VALUES (."'$post_name'"., ."'$post_text'".)";
 
-?>
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
