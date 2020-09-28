3d44e38247e3694bd78ed8dff1f254cacd52eac8
prokurists <71398178+prokurists@users.noreply.github.com>
2020-09-28 16:07:30 +0300

    Update index.php

--- resources/views/index.php
+++ resources/views/index.php
@@ -20,6 +20,19 @@ if ($result->num_rows > 0) {
   echo "0 results";
 }
 
+
+function deletePost($data){
+    $delete_post = "DELETE FROM posts WHERE id='".$data."'";
+    if ($conn->query($delete_post) === TRUE) {
+  echo "Record deleted successfully";
+} else {
+  echo "Error deleting record: " . $conn->error;
+}
+
+$conn->close();
+
+}
+
 ?>
 
 
