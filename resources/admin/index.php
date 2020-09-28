181e712e095de33b4272e25f9d80d8bb9ae447d3
prokurists <71398178+prokurists@users.noreply.github.com>
2020-09-28 13:54:04 +0300

    Update index.php

--- resources/admin/index.php
+++ resources/admin/index.php
@@ -1,26 +1,11 @@
-
-<form name="new_post"  method="POST">
-  <div class="form-group">
-    <label for="text">Posta nosaukums</label>
-    <input type="text" name="name" class="form-control">
-  </div>
-  <div class="form-group">
-    <label for="textarea">Teksts</label>
-    <textarea class="form-control" name="post_text" rows="5"></textarea>
-  </div> 
-  <button type="submit" class="btn btn-primary" name="new_post">Submit</button>
-</form>
 <?php
-if (isset($_POST["new_post"])){
-    $post_name = test_input($_POST["name"]);
-    $post_text = test_input($_POST["post_text"]);
-  
-$stmt = $conn->prepare("INSERT INTO posts (name, text) VALUES (?, ?)");
-$stmt->bind_param("ss", $post_name, $post_text);
 
-$stmt->execute();
-  echo "New records created successfully";
-$stmt->close();
-$conn->close();
+if (isset($_SESSION["adminSession"])){
+	require(../resources/admin/new_post.php);}
+else{
+	require(../resources/admin/admin_login.php;}
 
-}
+
+?>
+
+
