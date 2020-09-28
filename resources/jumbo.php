35306f4482a5ced29c26d4917595c8e1aca21606
prokurists <71398178+prokurists@users.noreply.github.com>
2020-09-28 14:46:32 +0300

    Update jumbo.php

--- resources/jumbo.php
+++ resources/jumbo.php
@@ -4,4 +4,14 @@
 </div>
 
 <?php 
-echo $alert; ?>
+if ($alert != ""){
+  echo "
+  <div class='alert alert-warning alert-dismissible fade show' role='alert'>
+  <strong>Kļūda!</strong>" . $alert. "
+  <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+    <span aria-hidden='true'>&times;</span>
+  </button>
+</div>
+  ";
+
+}?>
