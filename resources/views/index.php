bd90b00bed4047b8c34d0d00f4224e15b32a22bc
prokurists <71398178+prokurists@users.noreply.github.com>
2020-09-28 12:34:29 +0300

    Update index.php

--- resources/views/index.php
+++ resources/views/index.php
@@ -1,14 +1,21 @@
-<div class="card">
-<div class="card-body">
-    <h5 class="card-title">Pirmā pieredze programmēšanā</h5>
-    <p class="card-text"><?php require ("posts.php"); ?></p>
-    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-</div>
-</div><br><br>
-<div class="card">
-<div class="card-body">
-    <h5 class="card-title">Pirmā pieredze programmēšanā</h5>
-    <p class="card-text"><?php require ("posts.php"); ?></p>
-    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-</div>
-</div><br><br>
+<?php
+
+$sql = "SELECT user, name, text, date FROM posts WHERE active = 1";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  while($row = $result->fetch_assoc()) {
+    echo "<div class='card'>
+<div class='card-body'>
+    <h5 class='card-title'>" . $row["name"]. "</h5>
+    <p class='card-text'>" . $row["text"]. "</p>
+    <p class='card-text'><small class='text-muted'>" . $row["date"]. "</small></p>
+</div><br>";
+  }
+} else {
+  echo "0 results";
+}
+
+?>
+
+
