1c60b35c5559abf66c7ea11f59772295851dd6a6
prokurists <71398178+prokurists@users.noreply.github.com>
2020-09-28 12:31:00 +0300

    Update posts.php

--- resources/views/posts.php
+++ resources/views/posts.php
@@ -1,7 +1,14 @@
-Laurem ipsumLaurem ipsumLaurem ipsumLaurem ipsumLaurem remm
-ipsumLaurem ipsumLaurem ipsumLaurem ipsumLaurem ipsumLaurem
-ipsumLaurem ipsumLaurem ipsumLaurem ipsumLaurem ipsumLaurem 
-ipsumLaurem ipsumLaurem ipsumLaurem ipsumLaurem ipsumLaurem 
-ipsumLaurem ipsumLaurem ipsumLaurem ipsumLaurem ipsumLaurem 
-ipsumLauremipsumLaurem ipsumLaurem ipsumLaurem ipsumLauremm
-ipsumLaurem ipsumLaurem ipsumLaurem ipsumLaurem ipsumremrem
+<?php
+
+$sql = "SELECT user, name, text, date FROM posts WHERE active = 1";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  while($row = $result->fetch_assoc()) {
+    echo "User: " . $row["user"]. " - Post name:" . $row["name"]. " " . $row["text"]. " " . $row["date"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+
+?>
