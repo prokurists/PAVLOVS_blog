7b2c97c2f6ab56d3acac4cd27e565335a93172fb
prokurists <71398178+prokurists@users.noreply.github.com>
2020-09-28 13:54:19 +0300

    Update new_post.php

--- resources/admin/new_post.php
+++ resources/admin/new_post.php
@@ -1,8 +1,25 @@
+<form name="new_post"  method="POST">
+  <div class="form-group">
+    <label for="text">Posta nosaukums</label>
+    <input type="text" name="name" class="form-control">
+  </div>
+  <div class="form-group">
+    <label for="textarea">Teksts</label>
+    <textarea class="form-control" name="post_text" rows="5"></textarea>
+  </div> 
+  <button type="submit" class="btn btn-primary" name="new_post">Submit</button>
+</form>
 <?php
-  if ($_SERVER["REQUEST_METHOD"] == "POST"){
-    $name = test_input($_POST["name"]);
-    $text_area = test_input($_POST["post_text"]);
+if ((isset($_POST["new_post"]) && isset($_SESSION["adminSession"])){
+    $post_name = test_input($_POST["name"]);
+    $post_text = test_input($_POST["post_text"]);
+  
+$stmt = $conn->prepare("INSERT INTO posts (name, text) VALUES (?, ?)");
+$stmt->bind_param("ss", $post_name, $post_text);
 
-    echo "Paldies, " . $name . ". Tavs teksts: " . $text_area . " ir nosūtīts!"; }
+$stmt->execute();
+  echo "Pievienots jauns ieraksts";
+$stmt->close();
+$conn->close();
 
-?>
+}
