0c41ec26dffa7c487c2effb142a79a0a8ecc6e65
prokurists <71398178+prokurists@users.noreply.github.com>
2020-09-28 15:41:03 +0300

    Update index.php

--- resources/views/index.php
+++ resources/views/index.php
@@ -10,7 +10,7 @@ if ($result->num_rows > 0) {
 <div class='card-body'>
     <a href='#' onclick='";
     deletePost($row["id"]);
-    echo"' class='close'>&times;</a>
+    echo "' class='close'>&times;</a>
     <h5 class='card-title'>" . $row["name"]. "</h5>
     <p class='card-text'>" . $row["text"]. "</p>
     <p class='card-text'><small class='text-muted'>" . $row["date"]. "</small></p>
