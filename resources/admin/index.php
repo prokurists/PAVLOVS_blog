d23745bcbde837f793100f132bd854bc0ad1f123
prokurists <71398178+prokurists@users.noreply.github.com>
2020-09-28 14:07:24 +0300

    Update index.php

--- resources/admin/index.php
+++ resources/admin/index.php
@@ -1,9 +1,9 @@
 <?php
 
 if ($_SESSION["admin_Session"] == "admin"){
-	require __DIR__ . '/new_post.php';}
+	include __DIR__ . '/new_post.php';}
 else{
-	require __DIR__ . '/admin_login.php';}
+	include __DIR__ . '/admin_login.php';}
 
 
 ?>
