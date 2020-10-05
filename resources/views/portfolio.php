d9533ecda6d3285a513988514cc832ba66440d19
prokurists <71398178+prokurists@users.noreply.github.com>
2020-10-05 15:28:59 +0300

    .
    .

--- resources/views/portfolio.php
+++ resources/views/portfolio.php
@@ -1,32 +1,12 @@
-<div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
-  <ol class="carousel-indicators">
-    <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-    <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
-  </ol>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="../resources/images/pavlovs.JPG" class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>www.pavlovs.lv</h5>
-        <p>Alekseja Pavlova portfolio</p>
-      </div>
-    </div>
-    <div class="carousel-item">
-      <img src="../resources/images/izdevumi.png" class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>www.izdevumi.pavlovs.lv</h5>
-        <p>Top lietotne izdevumu sekošanai</p>
-      </div>
-    </div>
-
+<div class="card">
+  <div class="card-header">Portfolio</div>
+  <div class="card-body">LauremIpsum LauremIpsum LauremIpsum LauremIpsum LauremIpsum
+LauremIpsum LauremIpsum LauremIpsum LauremIpsum LauremIpsum LauremIpsum LauremIpsum LauremIpsum
+LauremIpsum LauremIpsum LauremIpsum LauremIpsum LauremIpsum LauremIpsum LauremIpsum LauremIpsum
+LauremIpsum LauremIpsum LauremIpsum LauremIpsum LauremIpsum LauremIpsum LauremIpsum LauremIpsum
+LauremIpsum LauremIpsum LauremIpsum LauremIpsum LauremIpsum LauremIpsum LauremIpsum LauremIpsum
+LauremIpsum LauremIpsum LauremIpsum LauremIpsum LauremIpsum LauremIpsum LauremIpsum LauremIpsum
+LauremIpsum LauremIpsum LauremIpsum LauremIpsum LauremIpsum LauremIpsum LauremIpsum LauremIpsum
   </div>
-  <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
+</div>
 </div>
