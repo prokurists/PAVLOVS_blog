967885580362f314c61942ccd2c9d07706f621da
prokurists <71398178+prokurists@users.noreply.github.com>
2020-10-05 15:24:27 +0300

    .
    .

--- resources/views/about.php
+++ resources/views/about.php
@@ -4,22 +4,22 @@
       <p class="card-text">Izglītība</p>
     </div>
     <div class="card-body">
-      <p>Pamatizglītība: Rīgas 84.vidusskola<br>
+      <p><strong>Pamatizglītība: Rīgas 84.vidusskola</strong><br>
       Studiju ilgums:	2002 - 2011</p>
 
-      <p>Vidējā izglītība: Rīgas 84.vidusskola<br>
+      <p><strong>Vidējā izglītība: Rīgas 84.vidusskola</strong><br>
       Studiju ilgums:	2011 - 2014</p>
 
-      <p>Papildu izglītība:<br>
-      Mācības pieaugušajiem VIAA projekts<br>
+      <p><strong>Papildu izglītība:</strong><br>
+      <i>Mācības pieaugušajiem VIAA projekts</i><br>
       Tēma: Web portālu izstrāde. Modernās tīmekļa tehnoloģijas programmatūras izstrādei<br></p>
-      <p>Gadgets<br>
+      <p><i>Gadgets</i><br>
       Tēma:	Upselling mazumtirdzniecībā, 2018<br></p>
-      <p>Gadgets<br>
+      <p><i>Gadgets</i><br>
       Tēma:	Pārdošanas prasmju treniņš, 2017<br></p>
-      <p>Gadgets<br>
+      <p><i>Gadgets</i><br>
       Tēma:	Efektīvs darbs ar klientiem, 2016<br></p>
-      <p>Gadgets<br>
+      <p><i>Gadgets</i><br>
       Tēma:	Ekspertu ēras pārdošana, 2016</p>
 
     </div>
