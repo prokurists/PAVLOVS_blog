d597ccddd93a4a6ba02a704871f4e667eef52c47
prokurists <71398178+prokurists@users.noreply.github.com>
2020-10-05 15:16:20 +0300

    .
    .

--- resources/views/about.php
+++ resources/views/about.php
@@ -1,22 +1,26 @@
 <div class="card-deck">
   <div class="card bg-primary">
-    <div class="card-body text-center">
+    <div class="card-header text-center">
       <p class="card-text">Izglītība</p>
     </div>
+    <div class="card-body">LauremIpsum LauremIpsum LauremIpsum Izglītība</div>
   </div>
   <div class="card bg-warning">
-    <div class="card-body text-center">
+    <div class="card-header text-center">
       <p class="card-text">Darba pieredze</p>
     </div>
+    <div class="card-body">LauremIpsum LauremIpsum LauremIpsum Darba pieredze </div>
   </div>
   <div class="card bg-success">
-    <div class="card-body text-center">
+    <div class="card-header text-center">
       <p class="card-text">Hobiji</p>
     </div>
+    <div class="card-body">LauremIpsum LauremIpsum LauremIpsum Hobiji</div>
   </div>
   <div class="card bg-danger">
-    <div class="card-body text-center">
+    <div class="card-header text-center">
       <p class="card-text">Pieredze programmēšanā</p>
     </div>
+    <div class="card-body">LauremIpsum LauremIpsum LauremIpsum Pieredze programmēšanā</div>
   </div>
 </div>
