7167ae075655a53ec55d36522531d49ef7e81713
prokurists <71398178+prokurists@users.noreply.github.com>
2020-10-05 15:21:20 +0300

    .
    .

--- resources/views/about.php
+++ resources/views/about.php
@@ -3,7 +3,26 @@
     <div class="card-header text-center">
       <p class="card-text">Izglītība</p>
     </div>
-    <div class="card-body">LauremIpsum LauremIpsum LauremIpsum Izglītība</div>
+    <div class="card-body">
+      <p>Pamatizglītība: Rīgas 84.vidusskola<br>
+      Studiju ilgums:	2002 - 2011</p>
+
+      <p>Vidējā izglītība: Rīgas 84.vidusskola<br>
+      Studiju ilgums:	2011 - 2014</p>
+
+      <p>Papildu izglītība:<br>
+      Mācības pieaugušajiem VIAA projekts<br>
+      Tēma: Web portālu izstrāde. Modernās tīmekļa tehnoloģijas programmatūras izstrādei<br>
+      Gadgets<br>
+      Tēma:	Upselling mazumtirdzniecībā, 2018<br>
+      Gadgets<br>
+      Tēma:	Pārdošanas prasmju treniņš, 2017<br>
+      Gadgets<br>
+      Tēma:	Efektīvs darbs ar klientiem, 2016<br>
+      Gadgets<br>
+      Tēma:	Ekspertu ēras pārdošana, 2016</p>
+
+    </div>
   </div>
   <div class="card bg-dark text-white">
     <div class="card-header text-center">
