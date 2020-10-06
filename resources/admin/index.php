4c48f1fa64bc695d8943fde19a3fcd364babafbf
prokurists <71398178+prokurists@users.noreply.github.com>
2020-10-06 22:35:22 +0300

    ..
    ...

--- resources/admin/index.php
+++ resources/admin/index.php
@@ -8,7 +8,7 @@ if(isset($_COOKIE['user'])) {
 	<li class='nav-item'><a class='nav-link' href='/admin/logout'>Logout</a></li></ul>";	
 
 	} else {
-		header('Location: http://www.pavlovs.lv/admin/login');
+        require __DIR__. '/admin/admin_login.php';
 	}
 
 
