6b68bbd8d770ccd1972a28193c91961a43c8b8b8
prokurists <71398178+prokurists@users.noreply.github.com>
2020-10-06 20:08:24 +0300

    .
    .

--- resources/views/post.php
+++ resources/views/post.php
@@ -1,3 +1,18 @@
 <?php
-    echo $number;
+    $sql = "SELECT id, user, name, text, date FROM posts WHERE id = ".$number."";
+    $result = $conn->query($sql);
+    
+    if ($result->num_rows > 0) {
+      while($row = $result->fetch_assoc()) {
+        echo "
+        <div class='card'>
+    <div class='card-body'>
+        <h5 class='card-title'>" . $row["name"]. "</h5>
+        <p class='card-text'>" . $row["text"]. "</p>
+        <p class='card-text'><small class='text-muted'>Ievietots: " . $row["date"]. " Ievietoja: " . $row["user"]. "</small></p>
+    </div></div><br>";
+      }
+    } else {
+      echo "0 results";
+    }
 ?>
