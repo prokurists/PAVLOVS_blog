eb53ef1b36b2d02cfd41e9f71af31c9f249942f5
prokurists <71398178+prokurists@users.noreply.github.com>
2020-10-06 22:25:17 +0300

    ..
    ..

--- resources/admin/index.php
+++ resources/admin/index.php
@@ -2,12 +2,10 @@
 
 if(isset($_COOKIE['user'])) {
 
-	echo "<li class='nav-item'>
+	echo "<ul class='navbar-nav mr-auto mt-2 mt-lg-0'>
 	<li class='nav-item'><a class='nav-link' href='/post/new'>New Post</a></li>
 	<li class='nav-item'><a class='nav-link' href='/post/delete'>Delete Post</a></li>
-	<li class='nav-item'><a class='nav-link' href='/admin/logout'>Logout</a></li></ul>";
-
-	
+	<li class='nav-item'><a class='nav-link' href='/admin/logout'>Logout</a></li></ul>";	
 
 	} else {
 		header('Location: http://www.pavlovs.lv/admin/login');
