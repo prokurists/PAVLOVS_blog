7dd26f465bb99edcfc6ce87cc2133588d85522d7
prokurists <71398178+prokurists@users.noreply.github.com>
2020-10-07 01:18:19 +0300

    m
    m

--- resources/views/contact.php
+++ resources/views/contact.php
@@ -1,4 +1,15 @@
-<form name="new_email" id="contactUs" method="POST" action="#">
+<?php
+  $first_Number = rand(0,10);
+  $second_Number = rand(0,10);
+  $total_Sum = $first_Number + $second_Number;
+?>
+
+<script>
+var total_real = "<?php echo $total_Sum; ?>";
+
+</script>
+
+<form name="new_email" id="contactUs"  method="POST">
   <div class="form-group">
     <label for="email">E-pasta adrese</label>
     <input type="email" name="email" class="form-control">
@@ -12,4 +23,23 @@
   <input type="number" name="total_sum_client" class="form-control">  <br>
   <button type="submit" onclick="return check_Form()" class="btn btn-primary" name="new_email">Submit</button>
 </form>
+
+<?php
+
+if (isset($_POST["new_email"])){
+  $email = test_input($_POST["email"]);
+  $text_area = test_input($_POST["textarea"]);
+
+  $stmt = $conn->prepare("INSERT INTO contacts (email, text) VALUES (?, ?)");
+    $stmt->bind_param("ss", $email, $text_area);
+
+    $stmt->execute();
+    $resMessage = array(
+      "status" => "alert-success",
+      "message" => "Your message is sent!");
+    $stmt->close();
+    $conn->close();
+}
+  
+?>
 
