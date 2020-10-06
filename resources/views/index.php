c127b2992c1cef1327fc33a691d3a575b5292a33
prokurists <71398178+prokurists@users.noreply.github.com>
2020-10-07 00:15:19 +0300

    .
    .

--- resources/views/index.php
+++ resources/views/index.php
@@ -1,3 +1,4 @@
+<div class='row no-gutters position-relative'>
 <?php
 
     $sql = "SELECT id, user, name, text, date FROM posts WHERE active = 1 ORDER BY id DESC";
@@ -6,18 +7,18 @@
   if ($result->num_rows > 0) {
       while($row = $result->fetch_assoc()) {
     echo "
-    <div class='media position-relative'>
-    <img src='../resources/images/1.jpg' class='mr-3' id='images' alt=''>
-    <div class='media-body'>
+    <div class='col-md-4 mb-md-0 p-md-4'>
+      <img src='../resources/images/1.jpg' id='images' class='w-100' alt=''>
+    </div> 
+    <div class='col-md-8 position-static p-4 pl-md-0'>
       <h5 class='mt-0'>" . $row["name"]. "</h5>
       <p>" . $row["text"]. "</p>
       <a href='". $row["id"]."' class='stretched-link'>Read more</a>
     </div>
-  </div>
   ";
   }
     } else {
   echo "No posts right now...";}
 
 
-?>
+?></div>
