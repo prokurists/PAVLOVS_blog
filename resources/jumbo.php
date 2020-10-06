7dfd8836591974fc31292784e5f5c349105b752a
prokurists <71398178+prokurists@users.noreply.github.com>
2020-10-07 01:23:31 +0300

    .
    .

--- resources/jumbo.php
+++ resources/jumbo.php
@@ -4,8 +4,11 @@
 </div>
 
 <?php if(!empty($resMessage)) {?>
-    <div class="alert <?php echo $resMessage['status']?>">
+    <div class="alert <?php echo $resMessage['status']?> alert-dismissible fade show' role='alert'">
       <?php echo $resMessage['message']?>
+      <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+      <span aria-hidden='true'>&times;</span>
+      </button>
     </div>
     <?php }?>
   </div>
