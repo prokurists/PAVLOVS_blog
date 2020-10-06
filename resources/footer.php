617fcaee65b7fbfdef8eb692971b71ee3dd3b32c
prokurists <71398178+prokurists@users.noreply.github.com>
2020-10-06 14:10:22 +0300

    .
    .

--- resources/footer.php
+++ resources/footer.php
@@ -1,7 +1,3 @@
 <p>Autors: Aleksejs Pavlovs</p>
 <p><a href="mailto:info@pavlovs.lv">aleksejs@pavlovs.lv</a></p>
 <a href="">Facebook</a> | <a href="">Whatsapp</a> | <a href="">LinkedIn</a> | <a href="">Instagram</a> |
-<?php if (isset($_COOKIE["user"])){
-  echo "<a href='/logout'>Izlogoties</a>"; }
-else{
-  echo "<a href='/admin'>Ielogoties</a>";} ?>
--- resources/navi.php
+++ resources/navi.php
@@ -11,5 +11,11 @@
       <a class="nav-link" href="/contact">Sakontaktēties</a>
     </li>
     </ul>
+
+    <?php if (isset($_COOKIE["user"])){
+      echo "<li class='nav-item'><a class='nav-link' href='/logout'>Izlogoties</a></li>"; }
+    else{
+      echo "<li class='nav-item'><a class='nav-link' href='/admin'>Admin panelis</a</li>>";} ?>
+
   </div>
 </nav>
