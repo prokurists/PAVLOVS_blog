84a8b7540e2898b901c2ed900e80b341abeaf09e
prokurists <71398178+prokurists@users.noreply.github.com>
2020-10-06 22:56:22 +0300

    ,
    ,

--- resources/admin/admin_logout.php
+++ resources/admin/admin_logout.php
@@ -2,6 +2,8 @@
 <?php
 
 setcookie("user", "", time() - 3600);
+header( "refresh:5;Location: http://www.pavlovs.lv" );
+
 
 
 ?>
--- resources/main.php
+++ resources/main.php
@@ -36,7 +36,6 @@ switch ($request) {
         break;
 
     default:
-        http_response_code(404);
         require __DIR__ . '/views/404.php';
         break;
 }
