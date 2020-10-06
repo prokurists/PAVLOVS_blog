b1fd7d7ee046d5f3b906df616a1ce2e7d98aed40
prokurists <71398178+prokurists@users.noreply.github.com>
2020-10-07 01:25:22 +0300

    .
    .

--- resources/jumbo.php
+++ resources/jumbo.php
@@ -4,29 +4,11 @@
 </div>
 
 <?php if(!empty($resMessage)) {?>
-    <div class="alert <?php echo $resMessage['status']?> alert-dismissible fade show' role='alert'">
-      <?php echo $resMessage['message']?>
-      <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
-      <span aria-hidden='true'>&times;</span>
-      </button>
-    </div>
-    <?php }?>
-  </div>
-
-  
-<?php
-function checkError($alert){
-if ($alert == ""){
-  // nekas nenotiek
-}
-else{
-  echo "
-  <div class='alert alert-warning alert-dismissible fade show' role='alert'>
-  <strong>Kļūda!</strong>" . $alert. "
+  <div class='alert <?php echo $resMessage['status']?> alert-dismissible fade show' role='alert'>
+  <?php echo $resMessage['message']?>
   <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
     <span aria-hidden='true'>&times;</span>
   </button>
 </div>
-  ";
-
-}}?>
+    <?php }?>
+  </div>
