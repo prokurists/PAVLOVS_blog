b1a172ba3e4e6877ca2fd76f80673081520e34c9
prokurists <71398178+prokurists@users.noreply.github.com>
2020-10-06 23:51:20 +0300

    .
    .

--- resources/views/post.php
+++ resources/views/post.php
@@ -7,12 +7,12 @@
       while($row = $result->fetch_assoc()) {
         echo "
         <div class='card'>
-    <div class='card-body'>
+    <div class='card-body' >
     <button type='button' class='close' onclick='deletePost(".$row["id"].")' aria-label='Close'>
     <span aria-hidden='true'>&times;</span>
   </button>
         <h5 class='card-title'>" . $row["name"]. "</h5>
-        <p class='card-text'>" . $row["text"]. "</p>
+        <p class='card-text' id='postFull'>" . $row["text"]. "</p>
         <p class='card-text'><small class='text-muted'>Ievietots: " . $row["date"]. " Ievietoja: " . $row["user"]. "</small></p>
     </div></div><br>";
       }
