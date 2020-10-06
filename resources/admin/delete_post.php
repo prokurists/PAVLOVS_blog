cb08464953fd4cdd3ed5273f09cc7eafa20e4ff3
prokurists <71398178+prokurists@users.noreply.github.com>
2020-10-06 23:14:14 +0300

    .
    .

--- resources/admin/delete_post.php
+++ resources/admin/delete_post.php
@@ -1,16 +1,38 @@
 <h1>Delete Post</h1>
 
 <?php
-    function deletePost($id){
 
-        $deletePostSql = "DELETE FROM posts WHERE id=".$id."";
 
-        if ($conn->query($deletePostSql) === TRUE) {
-          echo "Record deleted successfully";
+$sql = "SELECT id, name, FROM posts ORDER BY id DESC";
+    $result = $conn->query($sql);
+
+  if ($result->num_rows > 0) {
+      while($row = $result->fetch_assoc()) {
+    echo "<form action='/post/delete'>";
+    echo $row["name"];
+    echo "<input type='hidden' name='postId' value='". $row["id"]."'>";
+    echo "<input type='submit' name='postDelete' value='Submit'></form> "      
+  }
+    } else {
+  echo "No posts right now...";}
+
+
+
+  if (isset($_POST["postDelete"])){
+
+    $postId = $_POST["postId"];
+    $deletePost = "DELETE FROM posts WHERE id = '".$postId."'";
+    $result = mysqli_query($conn, $sql_exp_del);
+
+        if ($conn->query($deletePost) === TRUE) {
+            header('Location: ' . $_SERVER['HTTP_REFERER']);
         } else {
           echo "Error deleting record: " . $conn->error;
         }
         
         $conn->close();
     }
+  
+
+
 ?>
--- resources/admin/index.php
+++ resources/admin/index.php
@@ -2,10 +2,7 @@
 
 if(isset($_COOKIE['user'])) {
 
-	echo "<ul class='navbar-nav mr-auto mt-2 mt-lg-0'>
-	<li class='nav-item'><a class='nav-link' href='/post/new'>New Post</a></li>
-	<li class='nav-item'><a class='nav-link' href='/post/delete'>Delete Post</a></li>
-	<li class='nav-item'><a class='nav-link' href='/admin/logout'>Logout</a></li></ul>";	
+	echo "<h1>cPanel content</h1>";	
 
 	} else {
 		header('Location: http://www.pavlovs.lv/admin/login');
