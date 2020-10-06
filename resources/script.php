2f685fee79b7038d2a73454010cb150a29480e60
prokurists <71398178+prokurists@users.noreply.github.com>
2020-10-07 01:19:51 +0300

    ,
    ,

--- resources/script.php
+++ resources/script.php
@@ -1,5 +1,20 @@
 <?php
 
+if (isset($_POST["new_email"])){
+  $email = test_input($_POST["email"]);
+  $text_area = test_input($_POST["textarea"]);
+
+  $stmt = $conn->prepare("INSERT INTO contacts (email, text) VALUES (?, ?)");
+    $stmt->bind_param("ss", $email, $text_area);
+
+    $stmt->execute();
+    $resMessage = array(
+      "status" => "alert-success",
+      "message" => "Your message is sent!");
+    $stmt->close();
+    $conn->close();
+}
+
 function test_input($data) {
     $data = trim($data);
     $data = stripslashes($data);
