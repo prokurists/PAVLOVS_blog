e5bebe2a9f13fa46ff67b667dd78d359c4b9fd1d
prokurists <71398178+prokurists@users.noreply.github.com>
2020-10-06 22:05:51 +0300

    .
    .

--- resources/admin/index.php
+++ resources/admin/index.php
@@ -1,10 +1,12 @@
 <?php
 
 if(isset($_COOKIE['user'])) {
-	echo "<h1> Admin panelis </h1>"; }
-	else{
-    http_response_code(404);
-    require __DIR__ . '/views/404.php';}
+
+	echo "<h1> Admin panelis </h1>"; 
+
+	} else {
+		header('Location: http://www.pavlovs.lv/admin/login');
+	}
 
 
 ?>
