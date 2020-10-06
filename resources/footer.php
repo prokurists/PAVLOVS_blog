174de15120c4b58be7b358735a262e5927b83579
prokurists <71398178+prokurists@users.noreply.github.com>
2020-10-06 16:16:26 +0300

    .
    .

--- resources/footer.php
+++ resources/footer.php
@@ -1,3 +1,19 @@
 <p>Autors: Aleksejs Pavlovs</p>
 <p><a href="mailto:info@pavlovs.lv">aleksejs@pavlovs.lv</a></p>
 <a href="">Facebook</a> | <a href="">Whatsapp</a> | <a href="">LinkedIn</a> | <a href="">Instagram</a> |
+<?php
+if ($alert == ""){
+  // nekas nenotiek
+  echo "nav kludas";
+}
+else{
+  echo "
+  <div class='alert alert-warning alert-dismissible fade show' role='alert'>
+  <strong>Kļūda!</strong>" . $alert. "
+  <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+    <span aria-hidden='true'>&times;</span>
+  </button>
+</div>
+  ";
+
+}?>
