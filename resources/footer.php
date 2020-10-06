75f7e20cf022926290bc3b8ee6e8dc940adf3055
prokurists <71398178+prokurists@users.noreply.github.com>
2020-10-06 23:58:38 +0300

    ,
    ,

--- resources/footer.php
+++ resources/footer.php
@@ -1,23 +1,6 @@
 <p>Autors: Aleksejs Pavlovs</p>
 <p><a href="mailto:info@pavlovs.lv">aleksejs@pavlovs.lv</a></p>
-<a href="">Facebook</a> | <a href="">Whatsapp</a> | <a href="">LinkedIn</a> | <a href="">Instagram</a> |
 <a href="#" class="fa fa-facebook"></a>
-<a href="#" class="fa fa-twitter"></a>
 <a href="#" class="fa fa-google"></a>
 <a href="#" class="fa fa-linkedin"></a>
-<a href="#" class="fa fa-youtube"></a>
 <a href="#" class="fa fa-instagram"></a>
-<a href="#" class="fa fa-pinterest"></a>
-<a href="#" class="fa fa-snapchat-ghost"></a>
-<a href="#" class="fa fa-skype"></a>
-<a href="#" class="fa fa-android"></a>
-<a href="#" class="fa fa-dribbble"></a>
-<a href="#" class="fa fa-vimeo"></a>
-<a href="#" class="fa fa-tumblr"></a>
-<a href="#" class="fa fa-vine"></a>
-<a href="#" class="fa fa-foursquare"></a>
-<a href="#" class="fa fa-stumbleupon"></a>
-<a href="#" class="fa fa-flickr"></a>
-<a href="#" class="fa fa-yahoo"></a>
-<a href="#" class="fa fa-reddit"></a>
-<a href="#" class="fa fa-rss"></a>
