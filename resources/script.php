fb21905721072fc87e9ee1e95d4ab3ec70ac72bc
prokurists <71398178+prokurists@users.noreply.github.com>
2020-10-07 01:29:42 +0300

    .
    .

--- resources/script.php
+++ resources/script.php
@@ -11,6 +11,26 @@ var total_real = "<?php echo $total_Sum; ?>";
 
 <?php
 
+function showAllPosts($conn){
+  $sql = "SELECT id, user, name, text, date FROM posts WHERE active = 1 ORDER BY id DESC";
+  $result = $conn->query($sql);
+
+  if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+  echo "
+  <div class='col-md-4 mb-md-0 p-md-4'>
+  <img src='../resources/images/1.jpg' id='images' class='w-100' alt=''>
+  </div> 
+  <div class='col-md-8 position-static p-4 pl-md-0'>
+  <h5 class='mt-0'>" . $row["name"]. "</h5>
+  <p>" . $row["text"]. "</p>
+  <a href='". $row["id"]."' class='btn btn-primary stretched-link'>Read more</a>
+  <small class='text-muted'>Ievietots: " . $row["date"]. " Ievietoja: " . $row["user"]. "</small>
+  </div><hr>"; }
+  } else {  echo "No posts right now...";}}
+
+
+
 if (isset($_POST["new_email"])){
   $email = test_input($_POST["email"]);
   $text_area = test_input($_POST["textarea"]);
@@ -23,8 +43,7 @@ if (isset($_POST["new_email"])){
       "status" => "alert-success",
       "message" => "Your message is sent!");
     $stmt->close();
-    $conn->close();
-}
+    $conn->close();}
 
 function test_input($data) {
     $data = trim($data);
@@ -34,4 +53,11 @@ function test_input($data) {
   }
 
   
+
+
+
+
+
+
+
 ?>
--- resources/views/index.php
+++ resources/views/index.php
@@ -1,25 +1,6 @@
+  
 <div class='row no-gutters position-relative'>
-<?php
 
-    $sql = "SELECT id, user, name, text, date FROM posts WHERE active = 1 ORDER BY id DESC";
-    $result = $conn->query($sql);
+    <?php  showAllPosts(); ?> 
 
-  if ($result->num_rows > 0) {
-      while($row = $result->fetch_assoc()) {
-    echo "
-    <div class='col-md-4 mb-md-0 p-md-4'>
-      <img src='../resources/images/1.jpg' id='images' class='w-100' alt=''>
-    </div> 
-    <div class='col-md-8 position-static p-4 pl-md-0'>
-      <h5 class='mt-0'>" . $row["name"]. "</h5>
-      <p>" . $row["text"]. "</p>
-      <a href='". $row["id"]."' class='btn btn-primary stretched-link'>Read more</a>
-      <small class='text-muted'>Ievietots: " . $row["date"]. " Ievietoja: " . $row["user"]. "</small>
-      </div><hr>
-  ";
-  }
-    } else {
-  echo "No posts right now...";}
-
-
-?></div>
+</div>
